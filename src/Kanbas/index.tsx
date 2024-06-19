81d95487100d12a83897464ed340da7c130c4e37
NiyatiGohel <70020958+NiyatiGohel@users.noreply.github.com>
2024-06-19 15:41:26 -0400

    Update index.tsx

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -56,19 +56,18 @@ export default function Kanbas() {
             <Routes>
             <Route path="/Account/*" element={<Account />} />
               <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Dashboard" element={ 
+              <Route path="Dashboard" element={ <ProtectedRoute>
                 <Dashboard
                   courses={courses}
                   course={course}
                   setCourse={setCourse}
                   addNewCourse={addNewCourse}
                   deleteCourse={deleteCourse}
-                  updateCourse={updateCourse} />} />
-              <Route path="Courses/:cid/*" element={ 
-                <Courses courses={courses} /> } />
+                  updateCourse={updateCourse} /></ProtectedRoute>} />
+              <Route path="Courses/:cid/*" element={ <ProtectedRoute>
+                <Courses courses={courses} /></ProtectedRoute> } />
               <Route path="Calendar" element={<h1>Calendar</h1>} />
               <Route path="Inbox" element={<h1>Inbox</h1>} />
-              <Route path="Quiz" element={<QuizPreview />} />
             </Routes>
           </div>
         </div>
